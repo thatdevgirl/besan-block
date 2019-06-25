03b7253fab6cd53dc77e7707ef73068e85fde48e
Joni Halabi <her@thatdevgirl.com>
2019-06-25 15:06:54 -0400

    Adding horizontal bar graph rendering

--- besan-block/inc/render-block.inc.php
+++ besan-block/inc/render-block.inc.php
@@ -4,16 +4,26 @@
  */
 
 require_once( 'get-sheet-data.inc.php' );
+require_once( 'get-svg.inc.php' );
 
 function besan_render( $attributes, $content ) {
   // TODO: Make the user add this as an attribute so I'm not storing this key here.
   $api_key = esc_attr( 'AIzaSyDIxAtGtNO5Z43U6pND2YXhJzuysOsLoyY' );
 
-  $data = besan_get_sheet_data( $attributes, $api_key );
+  // Get the relevant data.
+  $raw_data = besan_get_sheet_data( $attributes, $api_key );
+  $data_body = json_decode( $raw_data['body'], true );
 
-  echo '<pre>';
-  print_r($data);
-  echo '</pre>';
+  // Find and count all of the unique values in the data.
+  $data = array();
 
-  return $attributes['data'];
+  foreach ( $data_body['values'] as $d ) {
+    if ( $data[ $d[0] ] ) {
+      $data[ $d[0] ]++;
+    } else {
+      $data[ $d[0] ] = 1;
+    }
+  }
+
+  return besan_get_svg( $data );
 }
